172a8a194c81ff7d080d3c7c657b4afb45cc7ad4
Jrma910121 <josemendez910121@gmail.com>
2026-03-19 19:12:44 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,38 @@ URLS = [
     "https://www.nike.com/t/air-max-dn8-leather-mens-shoes-GbnAW5Hb/IB6381-002",
     "https://www.nike.com/t/sb-air-max-95-skate-shoes-p6pzgr/HF7545-002",
     "https://www.nike.com/t/air-vapormax-plus-mens-shoes-nC0dzF/CK0900-001",
-    "https://www.nike.com/t/air-max-95-g-golf-shoes-pqM06obj/HV4696-002"
+    "https://www.nike.com/t/air-max-95-g-golf-shoes-pqM06obj/HV4696-002",
+    "https://www.nike.com/t/p-6000-shoes-XkgpKW/HF0015-002",
+    "https://www.nike.com/t/p-6000-shoes-XkgpKW/CD6404-026",
+    "https://www.nike.com/t/v5-rnr-mens-shoes-WHxi2GRN/II6292-004",
+    "https://www.nike.com/t/zoom-vomero-5-mens-shoes-MgsTqZ/HF1553-006",
+    "https://www.nike.com/t/zoom-vomero-5-mens-shoes-MgsTqZ/HF1553-006",
+    "https://www.nike.com/t/zoom-vomero-5-mens-shoes-MgsTqZ/HF1553-006",
+    "https://www.nike.com/t/zoom-vomero-5-mens-shoes-MgsTqZ/BV1358-003",
+    "https://www.nike.com/t/air-max-plus-mens-shoes-x9G2xF/IF4390-001",
+    "https://www.nike.com/t/air-max-plus-mens-shoes-x9G2xF/604133-050",
+    "https://www.nike.com/t/air-max-90-mens-shoes-bAZ6AeHT/IQ0300-001",
+    "https://www.nike.com/t/air-max-90-mens-shoes-bAZ6AeHT/CN8490-002",
+    "https://www.nike.com/t/air-max-excee-mens-shoes-vl97pm/FZ5486-006",
+    "https://www.nike.com/t/air-max-excee-mens-shoes-vl97pm/FN7304-001",
+    "https://www.nike.com/t/air-vapormax-plus-mens-shoes-nC0dzF/924453-004",
+    "https://www.nike.com/t/air-max-torch-4-mens-shoes-AKTdGZxX/343846-002",
+    "https://www.nike.com/t/zoom-vomero-5-se-sp-mens-shoes-MgsTqZ/CI1694-001",
+    "https://www.nike.com/t/zoom-vomero-5-se-sp-mens-shoes-MgsTqZ/BV1358-003",
+    "https://www.nike.com/t/zoom-vomero-5-se-sp-mens-shoes-MgsTqZ/FJ4151-007",
+    "https://www.nike.com/t/p-6000-shoes-XkgpKW/CN0149-001",
+    "https://www.nike.com/t/p-6000-shoes-XkgpKW/HF0015-002",
+    "https://www.nike.com/t/p-6000-shoes-XkgpKW/CD6404-026",
+    "https://www.nike.com/t/p-6000-shoes-XkgpKW/CD6404-028",
+    "https://www.nike.com/u/custom-nike-air-max-plus-by-you-10001901/4088545884",
+    "https://www.nike.com/t/air-max-plus-mens-shoes-x9G2xF/DM0032-030",
+    "https://www.nike.com/t/air-max-dn8-se-mens-shoes-YPsmAOxu/HV4525-001",
+    "https://www.nike.com/t/air-max-dn8-se-mens-shoes-YPsmAOxu/FQ7860-002",
+    "https://www.nike.com/t/air-force-1-gore-tex-vibram-mens-shoes-ASaVZlAr/HV5953-100",
+    "https://www.nike.com/t/air-force-1-07-mens-shoes-jBrhbr/CT2302-100",
+    "https://www.nike.com/t/nocta-air-force-1-low-mens-shoes-8T0Pt0/CZ8065-101",
+    "https://www.nike.com/t/dunk-low-gore-tex-mens-shoes-hbKMFmET/HQ2053-001",
+
 ]
 
 HEADERS = {
@@ -84,7 +115,7 @@ def main():
         nombre, precio = obtener_precio(url)
         print(f"DEBUG → {nombre} | Precio: {precio}")
 
-        if precio and precio < 100:
+        if precio and precio <= 180:
             encontrados.append(f"{nombre} - ${precio}\n{url}")
 
     if encontrados:
